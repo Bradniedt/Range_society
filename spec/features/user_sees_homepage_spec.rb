b6b1260c511a0de4ed8cbb263fc629a772cb684a
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-11 17:05:13 -0700

    updates test for visiting homepage feature test

--- spec/features/user_sees_homepage_spec.rb
+++ spec/features/user_sees_homepage_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 feature 'Visiting Homepage' do
   context 'as a visitor' do
-    it 'shows app name with tag line and synopsis' do
-
+    before :each do
+      visit root_path
+    end
+    it 'shows app name, tag line, and synopsis' do
+      expect(page).to have_content("Range Society")
+      expect(page).to have_content("Live Within Reach")
+      expect(page).to have_content("Discover locations and activities within your electric vehicle's range")
     end
     it 'show a button to sign in with google' do
-      
+      expect(page).to have_button("Sign in with Google")
     end
   end
 end
