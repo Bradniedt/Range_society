b380222ce0cec77592eee6f1ccb8a5e5078dbfed
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-11 17:01:25 -0700

    adds to gitignore and test setup

---
+++ spec/features/user_sees_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'Visiting Homepage' do
+  context 'as a visitor' do
+    it 'shows app name with tag line and synopsis' do
+
+    end
+    it 'show a button to sign in with google' do
+      
+    end
+  end
+end
