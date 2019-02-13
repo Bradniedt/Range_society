2a9cfcd571ba11f88717cf09bb6f08d63db40d9a
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-12 19:04:03 -0700

    added create route for search resource, paired commit with bradley

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/auth/google_oauth2', as: :google_login
   get '/auth/google_oauth2/callback', to: "sessions#create"
-  
-  resources :search, only: [:new]
+
+  resources :search, only: [:new, :create]
 end
