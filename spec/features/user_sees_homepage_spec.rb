51d8eb013aa28bef16a167e3ca73c83cc194e46b
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-13 08:18:05 -0700

    adds testing

--- spec/features/user_sees_homepage_spec.rb
+++ spec/features/user_sees_homepage_spec.rb
@@ -5,13 +5,9 @@ feature 'Visiting Homepage' do
     before :each do
       visit root_path
     end
-    xit 'shows app name, tag line, and synopsis' do
+    it 'shows app name, tag line, and synopsis' do
       expect(page).to have_content("Range Society")
       expect(page).to have_content("Live Within Reach")
-      expect(page).to have_content("Discover locations and activities within your electric vehicle's range")
-    end
-    xit 'show a button to sign in with google' do
-      expect(page).to have_button("Sign in with Google")
     end
   end
 end
