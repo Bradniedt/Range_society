148066e4adc0f94c7ef2b2ea040a5a8100deaf97
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-15 14:54:06 -0700

    adds updated getting lat and lon from session[:search]

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,11 +1,6 @@
 class MapController < ApplicationController
   def show
-    @search_query_url = search_query_url(session[:search])
-  end
-
-  private
-
-  def search_query_url(search)
-    "q=#{search["activity"]}&center=#{search["lat"]},#{search["lon"]}&zoom=14"
+    @lat = session[:search]["lat"]
+    @lng = session[:search]["lon"]
   end
 end
