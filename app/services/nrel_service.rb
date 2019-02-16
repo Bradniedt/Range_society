56505810df0d12314ce9083d635252562f9f69d9
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-16 10:04:05 -0700

    refactors NRel service class, all tests passing

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -6,7 +6,11 @@ class NrelService
   end
 
   def ev_charging_stations
-    conn = Faraday.new("https://developer.nrel.gov") do |faraday|
+    JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+  end
+
+  def conn
+    Faraday.new("https://developer.nrel.gov") do |faraday|
       faraday.params["api_key"] = ENV['NREL_API_KEY']
       faraday.params["fuel_type"] = "ELEC"
       faraday.params["limit"] = 25
@@ -15,11 +19,10 @@ class NrelService
       faraday.params["longitude"] = @lon
       faraday.adapter Faraday.default_adapter
     end
-
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json")
-    ev_stations = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
-    # ev_stations.each do |ev|
-    #   Ev_Station.new(ev)
-    # end
   end
+
+  def response
+    conn.get("/api/alt-fuel-stations/v1/nearest.json")
+  end
+
 end
