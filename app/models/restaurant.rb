85c44308c7eb4fa6af830b1363620ba7df1abb9d
Brad Niedt <bradleyniedt@gmail.com>
2019-02-15 21:03:28 -0700

    Adds changes to data hash calls.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,14 +9,14 @@ class Restaurant
               :phone,
               :distance
   def initialize(data)
-    @latitude = data[:coordinates][:latitude]
-    @longitude = data[:coordinates][:longitude]
-    @name = data[:name]
-    @street_address = data[:location][:address1]
-    @city = data[:location][:city]
-    @state = data[:location][:state]
-    @zip_code = data[:location][:zip_code]
-    @phone = data[:phone]
-    @distance = data[:distance]
+    @latitude = data["coordinates"]["latitude"]
+    @longitude = data["coordinates"]["longitude"]
+    @name = data["name"]
+    @street_address = data["location"]["address1"]
+    @city = data["location"]["city"]
+    @state = data["location"]["state"]
+    @zip_code = data["location"]["zip_code"]
+    @phone = data["phone"]
+    @distance = data["distance"]
   end
 end
