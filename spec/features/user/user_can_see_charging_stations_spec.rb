7d9783028dbe20b1645537b91d3b7395efd1baa2
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-16 14:39:53 -0700

    creates test that is most similar to how this will eventuall be executed, left comments above code showing that it is for testing purposes.

--- spec/features/user/user_can_see_charging_stations_spec.rb
+++ spec/features/user/user_can_see_charging_stations_spec.rb
@@ -10,11 +10,20 @@ describe 'as a logged in user, my path is search' do
     end
     it 'i see a map populated with results of ev stations within two miles of location', :vcr do
       User.first
+      range = "25"
+      zip = 81401
+      category = "Food"
 
-      visit ev_map_path
+      select("#{range}", :from => 'ev_range')
+      fill_in :search_location, with: zip
+      select("#{category}", :from => 'activity')
+      click_on("Search")
+      expect(current_path).to eq(map_path)
+
       lat = "39.7392"
       lon = "-104.9903"
-
+      fill_in :ev_search_location, with: "#{lat}, #{lon}"
+      click_button("Ev Search")
       expect(current_path).to eq(ev_map_path)
       expect(status_code).to eq(200)
     end
