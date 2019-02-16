38019e4f7f416dcd3b30ccff79fb07b7a419d9c8
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-16 15:24:59 -0700

    refactors Sessions#create to redirect to dashboard - also helper method to create a user if first time

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,19 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: request.env['omniauth.auth']["info"]["email"])
-    return redirect_to dashboard_path if user
+    found_user = User.find_by(email: request.env['omniauth.auth']["info"]["email"])
+    if found_user
+      flash[:notice] = "You're logged in #{found_user.first_name}"
+      session[:user_id] = found_user.id
+      redirect_to dashboard_path
+    else
+      create_user
+    end
+  end
 
-    user = User.from_omniauth(request.env['omniauth.auth']) unless user
+  private
+
+  def create_user
+    user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
       flash[:success] = "You're logged in #{user.first_name}"
       session[:user_id] = user.id
