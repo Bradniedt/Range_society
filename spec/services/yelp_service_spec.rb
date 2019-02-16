d619d4daf99c22d319b10febdd3d2c165cbb92ea
Brad Niedt <bradleyniedt@gmail.com>
2019-02-16 13:03:42 -0700

    Comments out vcr to try to get travis to pass.

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -7,11 +7,11 @@ describe YelpService do
     expect(yelp).to be_an_instance_of YelpService
   end
   it 'returns a collection of restaurants' do
-    VCR.use_cassette("yelp_restaurants") do
+    # VCR.use_cassette("yelp_restaurants") do
       rests = YelpService.get_restaurants("food", 39.7392, -104.9903)
-      
+
       expect(rests.count).to eq(25)
       expect(rests[0]).to be_an_instance_of Restaurant
-    end
+    # end
   end
 end
