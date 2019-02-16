4804ed92404549bb93d89987a06bb159814fb95c
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-16 12:04:37 -0700

    admin enable/disable user testing

---
+++ spec/features/admin/admin_can_enable_disable_user_spec.rb
@@ -0,0 +1,10 @@
+require 'rail_helper'
+
+describe 'as an Admin' do
+  
+end
+# as an admin, when i am logged in and visit /admin/users
+# i see all the users in the system
+# and there is an enable button by disabled users
+# and a disabled button by enabled users.
+# child of #25
