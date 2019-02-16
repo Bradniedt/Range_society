e11663f2ab1c0a5485105181b3711d8396ece41a
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-15 18:40:36 -0700

    adds current_admin helper method and fixes error in render 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_admin
+    true if current_user && current_user.admin?
+  end
+
   def render_404
-    render file: "#{RAILS_ROOT}/public/404.html", status: 404
+    render file: "./public/404.html", status: 404
   end
 end
