890febdac0370018cfab67d0235ca8f90f232636
Brad Niedt <bradleyniedt@gmail.com>
2019-02-15 20:07:15 -0700

    Adds change to yelp service test.

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -7,9 +7,9 @@ describe YelpService do
     expect(yelp).to be_an_instance_of YelpService
   end
   it 'returns a collection of restaurants' do
-    rests = YelpService.get_restaurants
+    rests = YelpService.get_restaurants("food", 39.7392, -104.9903)
 
     expect(rests.count).to eq(25)
     expect(rests[0]).to be_an_instance_of Restaurant
-  end 
+  end
 end
