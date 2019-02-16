ef87ce9b8646963b1e1928b134b2a8c254e95f2b
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-16 10:00:45 -0700

    adds another test for nrel service

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -2,8 +2,27 @@ require 'rails_helper'
 
 describe NrelService do
   it 'exists', :vcr do
-    ns = NrelService.new
+    lat = "39.7392"
+    lon = "-104.9903"
+    ns = NrelService.new(lat, lon)
 
     expect(ns).to be_a(NrelService)
+  end
+  it 'gets 25 or less ev charging stations', :vcr do
+    lat = "39.7392"
+    lon = "-104.9903"
+    ns = NrelService.new(lat, lon)
+    count = 25
+
+    expect(ns.ev_charging_stations.count).to eq(count)
+    expect(ns.ev_charging_stations.first).to have_key(:access_days_time)
+    expect(ns.ev_charging_stations.first).to have_key(:station_name)
+    expect(ns.ev_charging_stations.first).to have_key(:street_address)
+    expect(ns.ev_charging_stations.first).to have_key(:city)
+    expect(ns.ev_charging_stations.first).to have_key(:state)
+    expect(ns.ev_charging_stations.first).to have_key(:zip)
+    expect(ns.ev_charging_stations.first).to have_key(:distance)
+    expect(ns.ev_charging_stations.first).to have_key(:latitude)
+    expect(ns.ev_charging_stations.first).to have_key(:longitude)
   end
 end
