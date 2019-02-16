07262051b2c1c74d0963f6488e6fb09e40e9b3b0
Brad Niedt <bradleyniedt@gmail.com>
2019-02-16 14:48:39 -0700

    Adds change to yelp service.

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -10,18 +10,15 @@ class YelpService
   end
 
   def conn
-    Faraday.new("https://api.yelp.com/v3/businesses/search") do |f|
+    Faraday.new("https://api.yelp.com/") do |f|
       f.headers["Authorization"] = "Bearer #{ENV['YELP_API_KEY']}"
       f.adapter Faraday.default_adapter
     end
   end
 
   def get_restaurants
-    response = conn.get("?term=#{@activity}&latitude=#{@latitude}&longitude=#{@longitude}&limit=25")
+    response = conn.get("v3/businesses/search?term=#{@activity}&latitude=#{@latitude}&longitude=#{@longitude}&limit=25")
     results = JSON.parse(response.body)["businesses"]
-    results.map do |result|
-      Restaurant.new(result)
-    end
   end
 
 end
