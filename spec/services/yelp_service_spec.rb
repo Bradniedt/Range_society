5c6e4a8cfcb7668dd4271b3e141f52bbb6aa5e66
Brad Niedt <bradleyniedt@gmail.com>
2019-02-16 14:41:47 -0700

    Adds change to yelp service test to pull out functionality to a facade.

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -10,8 +10,17 @@ describe YelpService do
     # VCR.use_cassette("yelp_restaurants") do
       rests = YelpService.get_restaurants("food", 39.7392, -104.9903)
 
-      expect(rests.count).to eq(25)
-      expect(rests[0]).to be_an_instance_of Restaurant
+      expect(rests.first).to have_key("coordinates")
+      expect(rests.first["coordinates"]).to have_key("latitude")
+      expect(rests.first["coordinates"]).to have_key("longitude")
+      expect(rests.first).to have_key("name")
+      expect(rests.first).to have_key("location")
+      expect(rests.first["location"]).to have_key("address1")
+      expect(rests.first["location"]).to have_key("city")
+      expect(rests.first["location"]).to have_key("state")
+      expect(rests.first["location"]).to have_key("zip_code")
+      expect(rests.first).to have_key("phone")
+      expect(rests.first).to have_key("distance")
     # end
   end
 end
