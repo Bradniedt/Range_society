689e155aab5ce430f17b79626031648debfe0c8a
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-16 12:44:34 -0700

    adds env vars to travis.yaml

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -8,23 +8,21 @@ describe NrelService do
 
     expect(ns).to be_a(NrelService)
   end
-  describe '#instance methods' do
-    it 'raw_ev_charging_stations' do
-      lat = "39.7392"
-      lon = "-104.9903"
-      ns = NrelService.new(lat, lon)
-      count = 20
+  it 'returns hashes for charging stations', :vcr do
+    lat = "39.7392"
+    lon = "-104.9903"
+    ns = NrelService.new(lat, lon)
+    limit = 20
 
-      expect(ns.raw_ev_charging_stations.count).to eq(count)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:access_days_time)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:station_name)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:street_address)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:city)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:state)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:zip)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:distance)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:latitude)
-      expect(ns.raw_ev_charging_stations.first).to have_key(:longitude)
-    end
+    expect(ns.raw_ev_charging_stations.count).to eq(limit)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:access_days_time)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:station_name)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:street_address)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:city)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:state)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:zip)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:distance)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:latitude)
+    expect(ns.raw_ev_charging_stations.first).to have_key(:longitude)
   end
 end
