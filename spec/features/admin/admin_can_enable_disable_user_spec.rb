7ddcaf2f7894eba5d54e3cd60a81de15fd720c64
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-16 18:19:53 -0700

    remove commented out code

--- spec/features/admin/admin_can_enable_disable_user_spec.rb
+++ spec/features/admin/admin_can_enable_disable_user_spec.rb
@@ -31,8 +31,3 @@ describe 'as an Admin' do
     end
   end
 end
-# as an admin, when i am logged in and visit /admin/users
-# i see all the users in the system
-# and there is an enable button by disabled users
-# and a disabled button by enabled users.
-# child of #25
