5df2fdfeeaa55cf13d82761ae7fa9dbb25576318
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-17 16:50:19 -0700

    removes museum - add general place poro

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,33 @@
 class Place
+  attr_reader :name,
+              :image_url,
+              :is_closed,
+              :url,
+              :review_count,
+              :rating,
+              :coordinates,
+              :location,
+              :display_phone,
+              :distance
+
   def initialize(attributes)
-    
+    @name = attributes[:name]
+    @image_url = attributes[:image_url]
+    @is_closed = attributes[:is_closed]
+    @url = attributes[:url]
+    @review_count = attributes[:review_count]
+    @rating = attributes[:rating]
+    @coordinates = attributes[:coordinates]
+    @location = attributes[:location]
+    @display_phone = attributes[:display_phone]
+    @distance = attributes[:distance]
+  end
+
+  def display_address
+    location[:display_address].join(" ")
+  end
+
+  def popup_html
+    "<p><b>#{name}</b></p><b>Address:</b> #{display_address}</p><p><a href='#{url}'>View on Yelp</a></p>"
   end
 end
