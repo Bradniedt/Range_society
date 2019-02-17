20c0a51295169b01cd10b2fa2bdc74c287fadbde
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-17 16:51:47 -0700

    instantiates service in map controller and uses to get businesses

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,19 +1,8 @@
 class MapController < ApplicationController
   def show
-    conn = Faraday.new("https://api.yelp.com/v3/businesses/search") do |f|
-      f.adapter Faraday.default_adapter
-      f.headers["Authorization"] = "Bearer #{ENV['YELP_API_KEY']}"
-    end
-    response = conn.get do |req|
-      req.params['latitude'] = session[:search]["lat"]
-      req.params['longitude'] = session[:search]["lon"]
-      req.params['categories'] = session[:search]["activity"].downcase
-    end
-    museums_json = JSON.parse(response.body, symbolize_names: true)[:businesses]
-    @museums = museums_json.reduce([]) do |museums, raw_museum|
-      museums.push(Museum.new(raw_museum))
-    end
     @lat = session[:search]["lat"]
     @lon = session[:search]["lon"]
+    @service = YelpService.new(@lat, @lon)
+    @businesses = @service.businesses_search(session[:search]["activity"])
   end
 end
