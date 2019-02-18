4a0c229cb6fd66bf6a9c33eb00d73b569d2ba36f
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-18 13:04:23 -0700

    adds shoulda-matchers to the rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,12 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
