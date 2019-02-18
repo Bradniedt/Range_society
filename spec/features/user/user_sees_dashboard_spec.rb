5576dec764ae498b0d0ca584b00dff2a600b9b2d
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-18 10:43:26 -0700

    basic test for user to see dashboard with information

---
+++ spec/features/user/user_sees_dashboard_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'As a User' do
+  before(:each) do
+    visit root_path
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    @user_info = stub_omniauth
+    click_button "Sign in With Google"
+  end
+  it 'shows dashboard' do
+    visit dashboard_path
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Hello #{@user.first_name}")
+    expect(page).to have_content("#{@user.first_name}'s Dashboard")
+  end
+end
