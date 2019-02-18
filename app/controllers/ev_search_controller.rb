67233e47d84f913468cc67c309327a04aff2c3ec
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-18 11:25:52 -0700

    adds getting lat, long, and popup_html from req.body

--- app/controllers/ev_search_controller.rb
+++ app/controllers/ev_search_controller.rb
@@ -1,6 +1,12 @@
 class EvSearchController < ApplicationController
   protect_from_forgery with: :null_session
+  before_action :require_user
+
   def create
-    JSON.parse(request.body.read, symbolize_names: true)
+    picked_location = JSON.parse(request.body.read, symbolize_names: true)
+    latitude, longitude = picked_location[:coordinates].split(" ")
+    session[:ev_search] = {latitude: latitude, longitude: longitude}
+    session[:picked_location_html] = picked_location[:popup_html]
+    redirect_to ev_map_path
   end
 end
