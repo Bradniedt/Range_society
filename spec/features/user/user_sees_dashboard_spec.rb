70539c73437d73bac29c7b37f7d73781aa17253f
Brad Niedt <bradleyniedt@gmail.com>
2019-02-19 19:27:40 -0700

    Adds test for user seeing recent destinations.

--- spec/features/user/user_sees_dashboard_spec.rb
+++ spec/features/user/user_sees_dashboard_spec.rb
@@ -13,4 +13,35 @@ describe 'As a User' do
     expect(page).to have_content("#{@user_info.first_name}'s Dashboard")
     expect(page).to have_link("Create a Trip Log")
   end
+  it 'shows me my ten most recent destinations' do
+    user = User.last
+    place1 = user.destinations.create(name: "1place", location: "1location")
+    place2 = user.destinations.create(name: "2place", location: "2location")
+    place3 = user.destinations.create(name: "3place", location: "3location")
+    place4 = user.destinations.create(name: "4place", location: "4location")
+    place5 = user.destinations.create(name: "5place", location: "5location")
+    place6 = user.destinations.create(name: "6place", location: "6location")
+    place7 = user.destinations.create(name: "7place", location: "7location")
+    place8 = user.destinations.create(name: "8place", location: "8location")
+    place9 = user.destinations.create(name: "9place", location: "9location")
+    place10 = user.destinations.create(name: "10place", location: "10location")
+    place11 = user.destinations.create(name: "11place", location: "1l1ocation")
+
+    visit dashboard_path
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Most Recent Destinations:")
+    expect(page).to have_css('.destination', count: 10)
+    within('.recent_destinations') do
+      expect(page).to have_content(place2.name)
+      expect(page).to have_content(place3.name)
+      expect(page).to have_content(place4.name)
+      expect(page).to have_content(place5.name)
+      expect(page).to have_content(place6.name)
+      expect(page).to have_content(place7.name)
+      expect(page).to have_content(place8.name)
+      expect(page).to have_content(place9.name)
+      expect(page).to have_content(place10.name)
+      expect(page).to have_content(place11.name)
+    end 
+  end
 end
