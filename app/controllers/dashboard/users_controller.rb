bc413c049983f441c5fbbd293aca8721f70cb6ca
Brad Niedt <bradleyniedt@gmail.com>
2019-02-19 20:13:43 -0700

    Adds method to user controller to bring destinations into user dashboard and adds destinations section.

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,5 +1,6 @@
 class Dashboard::UsersController < Dashboard::BaseController
   def show
-    @user = User.find(session[:user_id])
+    @user = User.includes(:destinations).find(session[:user_id])
+    @destinations = @user.destinations.order(created_at: :desc).limit(10)
   end
 end
