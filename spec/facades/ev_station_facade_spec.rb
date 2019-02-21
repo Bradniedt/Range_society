b70a4bae28e780b22c492a57458d1058af0d9302
Mary Goodhart <mary.loves.pry@gmail.com>
2019-02-21 09:48:21 -0700

    this test again

--- spec/facades/ev_station_facade_spec.rb
+++ spec/facades/ev_station_facade_spec.rb
@@ -8,21 +8,13 @@ describe EvStationFacade do
 
     expect(evsf).to be_a(EvStationFacade)
   end
-  it 'ev_stations', :vcr do
-    lat = "39.7392"
-    lon = "-104.9903"
-    evsf = EvStationFacade.new(lat, lon)
-
-    expect(evsf.all_ev_stations.count).to eq(20)
-    expect(evsf.all_ev_stations.first).to be_a(EvStation)
-  end
   it 'public_ev_stations', :vcr do
     lat = "39.7392"
     lon = "-104.9903"
     evsf = EvStationFacade.new(lat, lon)
-    count = evsf.public_ev_stations.count
+    count = 20
 
-    expect(evsf.public_ev_stations.count).to eq(count)
-    expect(evsf.public_ev_stations.first).to be_a(EvStation)
+    expect(evsf.ev_stations.count).to eq(count)
+    expect(evsf.ev_stations.first).to be_a(EvStation)
   end
 end
