968222f573dd2d02c399d7a0448a479e5531ef23
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-01 10:14:54 -0700

    refactors ev_map to exclusively used cached facade

--- app/controllers/ev_map_controller.rb
+++ app/controllers/ev_map_controller.rb
@@ -3,11 +3,6 @@ class EvMapController < ApplicationController
   before_action :require_user
 
   def show
-    cached_facade = Cache.retrieve(:ev_facade, delete: true)
-    @ev_facade = cached_facade || EvStationFacade.new(
-      session[:ev_search]["latitude"],
-      session[:ev_search]["longitude"],
-      session[:picked_location_html]
-    )
+    @ev_facade = Cache.retrieve(:ev_facade)
   end
 end
