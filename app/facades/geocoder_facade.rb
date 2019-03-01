43792c3a116b278487e59d13f34b4df2bd688250
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-01 08:48:28 -0700

    adds Geocoder Facade

---
+++ app/facades/geocoder_facade.rb
@@ -0,0 +1,25 @@
+class GeocoderFacade
+  def get_lat_lon(location)
+    case location
+    when /\d{2}\.\d{7,16} -\d{3}\.\d{7,16}/ then
+      lat, lon = location.split(" ")
+      return {lat: lat, lon:lon}
+    when /^\d{5}\s*$/ then
+      # will need to change this eventually if we want this to work nationally.
+      result = service.search(location).select{ |place| place.data["address"]["state"] == "Colorado"}.first.data
+      lat = result["lat"]
+      lon = result["lon"]
+      return {lat: lat, lon:lon}
+    when /^$/ then
+      return nil
+    else
+      return false
+    end
+  end
+
+  private
+
+  def service
+    Geocoder
+  end
+end
